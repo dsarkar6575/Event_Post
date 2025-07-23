5166b29b31f069d1eded64f431a797fbea85583a
kartikcmandi222 <kartikcmandi222@gmail.com>
2025-07-23 12:38:41 +0000

    km 23 18:04

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider with ChangeNotifier {
-  ThemeMode _themeMode = ThemeMode.light;
+  ThemeMode _themeMode = ThemeMode.system;
 
   ThemeMode get themeMode => _themeMode;
 
-  void toggleTheme() {
-    _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+  static const String _themeKey = 'theme_mode';
+
+  ThemeProvider() {
+    _loadThemeFromPrefs();
+  }
+
+  void toggleTheme(bool isDark) {
+    _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
+    _saveThemeToPrefs();
     notifyListeners();
+  }
+
+  Future<void> _loadThemeFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    final theme = prefs.getString(_themeKey);
+
+    if (theme == 'light') {
+      _themeMode = ThemeMode.light;
+    } else if (theme == 'dark') {
+      _themeMode = ThemeMode.dark;
+    } else {
+      _themeMode = ThemeMode.system;
+    }
+
+    notifyListeners();
+  }
+
+  Future<void> _saveThemeToPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    final themeString = _themeMode == ThemeMode.dark
+        ? 'dark'
+        : _themeMode == ThemeMode.light
+            ? 'light'
+            : 'system';
+    await prefs.setString(_themeKey, themeString);
   }
 }
